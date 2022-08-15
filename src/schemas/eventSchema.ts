c24eba6f6f27d7bb5d315d558e7baf5940a4691c
felipemejias <felipe.mejias50@gmail.com>
2022-08-15 18:08:06 -0300

    forgot to put the time regex in event schema

--- src/schemas/eventSchema.ts
+++ src/schemas/eventSchema.ts
@@ -1,10 +1,12 @@
 import joi from 'joi'
 import { EventData } from '../services/eventService'
 
+const regex=/^([0-9]{2},[0-9]{2}|[0-9]{1},[0-9]{2}|[0-9]{2}|[0-9]{1})$/
+
 export const schema=joi.object<EventData>({
     title:joi.string().required(),
     day:joi.number().required(),
-    begin:joi.string().required(),
-    end:joi.string().required(),
+    begin:joi.string().pattern(regex).required(),
+    end:joi.string().pattern(regex).required(),
     chosen:joi.array()
 })
