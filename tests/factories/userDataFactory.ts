1a40d754829646c1025aea379b2216f7110f82d5
Felipe Mejias <felipe.mejias50@gmail.com>
2022-09-20 15:59:06 -0300

    Delete tests directory

--- tests/factories/userDataFactory.ts
+++
@@ -1,8 +0,0 @@
-import { faker } from "@faker-js/faker";
-
-export default function userBodyFactory() {
-  return {
-    name: faker.internet.userName(),
-    password: faker.internet.password(),
-  };
-}
