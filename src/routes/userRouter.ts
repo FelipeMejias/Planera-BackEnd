42863a87a7f750ccb00adcf6d79a34c021477827
felipemejias <felipe.mejias50@gmail.com>
2022-09-23 11:33:29 -0300

    editor monitor for users

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import { userController } from '../controllers/userController.js'
+import { prisma } from '../database.js'
 import { validateBody } from '../middlewares/validateBody.js'
 import { schema } from '../schemas/userSchema.js'
 
@@ -7,5 +8,17 @@ const userRouter=Router()
 
 userRouter.post('/signup',validateBody(schema),userController.signUp)
 userRouter.post('/signin',validateBody(schema),userController.signIn)
+userRouter.get('/usernames',async(req:any,res:any)=>{
+    try {
+        const {editor}=req.headers
+        if (editor!=='jhc777')return res.sendStatus(400)
+        const result=await prisma.user.findMany()
+        const filtered=result.map(obj=>obj.name)
+        res.status(200).send(filtered)
+    } catch (error) {
+        res.sendStatus(500)
+    }
+    
+})
 
 export default userRouter
